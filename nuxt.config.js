4c5274bfcf04b6b00c26b92fd9c592f1f9286efe
HarasawaNaoya <rururulily@gmail.com>
2018-10-30 01:09:13 +0900

    html head変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { name: 'viewport', content: 'width=900' },
+      { hid: 'description', name: 'smipaintpwa', content: 'すみぺいんとぴーだぶりゅーえー' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
--- static/sw.js
+++ static/sw.js
@@ -8,12 +8,12 @@ workbox.precaching.precacheAndRoute([
     "revision": "126822ea7014a53213c7b48d3fd01d81"
   },
   {
-    "url": "/smipaint/_nuxt/ac56db2c3485c73164f0.js",
-    "revision": "8da1d541638339d7ef704523ad1447a6"
+    "url": "/smipaint/_nuxt/9dbe6d7c840853855806.js",
+    "revision": "6c85c2f8dab8b88fd7777e958faed2ef"
   },
   {
-    "url": "/smipaint/_nuxt/c4edba9034d9c1fe3138.js",
-    "revision": "54fa1c873896c06a60e3adc928253dae"
+    "url": "/smipaint/_nuxt/ac56db2c3485c73164f0.js",
+    "revision": "8da1d541638339d7ef704523ad1447a6"
   },
   {
     "url": "/smipaint/_nuxt/ea105b548f493883ac28.js",
